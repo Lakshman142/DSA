f42bfbf85d49baf1eb3e454d99f921f1ca43b027
Lakshman Somu <96649441+Lakshman142@users.noreply.github.com>
2024-12-30 10:30:49 +0530

    Update Selection_Sort.py

--- Sorting_Algorithms/Selection_Sort.py
+++ Sorting_Algorithms/Selection_Sort.py
@@ -1,1 +1,11 @@
+def selectionSort(self, arr):
+    for i in range(len(arr)):
+        min_idx=i
+        for j in range(i+1,len(arr)):
+            if arr[j]<arr[min_idx]:
+                min_idx=j
+        arr[i],arr[min_idx]=arr[min_idx],arr[i]
+
+# Time Complexity: O(n*n)
+#Space Complexity: O(1)
 
